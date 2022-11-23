f36ea6d9fa06eb76d6a502e48c8ba805996b66ea
pele <lfeliualvarez@danielcastelao.org>
2022-11-23 14:03:56 +0100

    segundo commit

---
+++ Boletin2_2/src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin2_2;
+
+/**
+ *
+ * @author lucas
+ */
+public class Boletin2_2 {
+
+    public static void main(String[] args) {
+        //Proyecto que calcula el area de un cuadrado de lado=3
+        float lado = 3f, area;
+        area = lado * lado;
+        System.out.println("area =" + area);
+    }
+
+}
